ba16cccbdeadf1195c9cc3b20700ebe08a4e75ea
root <kavya.k@evolvussolutions.com>
2020-04-24 19:56:18 +0530

    inheritance chnages

--- JSCLASSES/Monkey_COnSTRUCTOROVERIDING.js
+++ JSCLASSES/Monkey_COnSTRUCTOROVERIDING.js
@@ -16,6 +16,11 @@ class Monkey extends Animal{
 
         console.log(`the animal ${this.name} is having weight ${this.wit} is singing`)
     }
+
+    static age(){
+
+        console.log('inside the Monkey class age')
+    }
 }
 
 let monkey=new Monkey('Monkey123',122)
@@ -23,3 +28,5 @@ let monkey=new Monkey('Monkey123',122)
 monkey.sing()
 
 Animal.age()
+
+Monkey.age()
